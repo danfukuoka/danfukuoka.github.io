098a7e9b524bc6fb0f4daf698bbc74092f3bc855
danfukuoka <danilofukuoka@gmail.com>
2025-04-30 09:45:29 -0300

    Updating k8s workshop

--- offsite-2025/k8s/app.py
+++ offsite-2025/k8s/app.py
@@ -1,24 +1,41 @@
 import pymongo
 import os
 import time
+from datetime import datetime, timezone
 
+# ENVIRONMENT VARIABLE
 uri = os.environ["MONGODB_URI"]
 
-properties = {"ENVIRONMENT": "k8s"}  # "/var/run/secrets/kubernetes.io/serviceaccount/token"
+# DATABASE CONNECTION
+properties = {"ENVIRONMENT": "k8s"}  
 client = pymongo.MongoClient(
    uri,
    authMechanism="MONGODB-OIDC",
    authMechanismProperties=properties
 )
 
+# APPLICATION LOGIC
 while True:
    
-   # Print the current JWT token.
-   print("TOKEN: ", open("/var/run/secrets/kubernetes.io/serviceaccount/token", "r").read() if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/token") else "Token file not found.")
+   # Get the current JWT token (for monitoring purposes).
+   token = open("/var/run/secrets/kubernetes.io/serviceaccount/token", "r").read() if os.path.exists("/var/run/secrets/kubernetes.io/serviceaccount/token") else "Token file not found."
    
-   # Print the secret from the database.
-   print("SECRET: ", client["secret-database"]["secret-collection"].find_one().get("secret", "Key 'secret' not found"))
+   # Print the token.
+   print(f"Current Token: {token}")
+   
+   # Prepare the data to be inserted.
+   document = {
+       "timestamp": datetime.now(timezone.utc),
+       "name": os.getenv('BONUSLY', 'Not Set'),
+       "token": token
+   }
 
+   # Insert the document into the bonusly-k8s collection.
+   client["bonusly-k8s"]["activity-log"].insert_one(document)
+   print(f"Inserted document: {document}")
+
+   print("CONGRATULATIONS! You have finished the lab. Workload Identity rocks! :)")
+   
    # Sleep for 10 minutes.
    print("Sleeping for 10 minutes. We expect that the token will be refreshed by the kubelet and that the MongoDB driver will automatically get the new token and access the database.")
-   time.sleep(600) 
+   time.sleep(600)
